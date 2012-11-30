96e12036b784cb9ffe4c482f838a35f1fc5403a1
Zahra Jabini <zahra.arch@gmail.com>
2012-11-30 15:42:40 -0500

    rails server and client made

---
+++ communikatr-client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunikatrClient::Application.config.secret_token = '0bb9e1bff11a39fa2ecc319b67010c99bc5cf16f35fbb4ac2be890151450bd835d324e9ca473754d9cffdaf265681e06c9c118c8ebb782be1ecd65351bd1f722'
---
+++ communikatr-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunikatrServer::Application.config.secret_token = '11e96236bef6552a3813e12c4d04731db97707f6d0e71ab44aa5d2d00601cf469800b675d0ee377bc251e51b61c1982c580588bc61f9b812d2eb8a9cec6339ae'
